b8946982303bc4071efc9b0dc0f6b0895477604c
Devansh Soni <devanshsoni79@gmail.com>
2025-03-11 20:03:37 +0000

    remaps

--- lua/crade/remap.lua
+++ lua/crade/remap.lua
@@ -9,13 +9,18 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 
+
+vim.keymap.set("n", "<leader>q", ":q<CR>")
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+
+
 --quickfix stuff
 
 local function clear_quickfix_list()
   vim.fn.setqflist({})
 end
 
-vim.api.nvim_create_user_command('ClearQuickfixList', clear_quickfix_list, {})
+api.nvim_create_user_command('ClearQuickfixList', clear_quickfix_list, {})
 
 vim.api.nvim_set_keymap('n', '<leader>cf', ':ClearQuickfixList<CR>', { noremap = true, silent = true })
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
@@ -35,8 +40,14 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true, silent = true, desc = 'Next buffer' })
 
 vim.keymap.set('n', '<leader>h', '<C-w>h', { noremap = true, silent = true, desc = 'Previous buffer' })
+vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true, silent = true, desc = 'Upper buffer' })
+
+vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true, silent = true, desc = 'Bottom buffer' })
 --splits
 vim.keymap.set("n", "<leader>v", ":vsplit<CR>")
+--resize
+vim.keymap.set("n", "<leader>,", "<C-w><")
+vim.keymap.set("n", "<leader>.", "<C-w>>")
 
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
@@ -45,11 +56,9 @@ vim.keymap.set("n", "<leader>b", "%");
 
 vim.keymap.set("n", "Q", "<nop>")
 -- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
--- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set({ "n", "v" }, "<S-E>", "$")
 
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
